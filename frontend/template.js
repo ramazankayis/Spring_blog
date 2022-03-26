af5586a53f4313bccc0f2ff9dd09cffef7e1a5fe
ramazankayis <kayis.ramazan@gmail.com>
2022-03-26 16:56:21 +0300

    edited register modal

--- frontend/template.js
+++ frontend/template.js
@@ -58,37 +58,37 @@ $(function () {
     // alert("Tıkladı")
 
     //input değerlerini almak
-    let nameInput, emailInput, passwordInput, telNumberInput;
+    let nameInput, emailInput, passwordInput, telNumberInput
     nameInput = $('#name_surname').val();
-    nameInput = jQuery.trim(nameInput);
+    // nameInput = jQuery.trim(nameInput);
 
-    emailInput = $('#email').val();
-    emailInput = jQuery.trim(emailInput);
+    emailInput = $('#email_r').val();
+    // emailInput = jQuery.trim(emailInput);
 
-    passwordInput = $('#password').val();
-    passwordInput = jQuery.trim(passwordInput);
+    passwordInput = $('#password_r').val();
+    // passwordInput = jQuery.trim(passwordInput);
 
     telNumberInput = $('#tel_number').val();
-    telNumberInput = jQuery.trim(telNumberInput);
+    // telNumberInput = jQuery.trim(telNumberInput);
 
     //validata data
     //validation_name , validation_email  , validation_password , validation_number
-    let validationName, validationEmail, validationPassword, validationNumber;
-    validationName = $('#validation_name');
-    validationEmail = $('#validation_email');
-    validationPassword = $('#validation_password');
-    validationNumber = $('#validation_number');
-
+    let validationName, validationEmail, validationPassword, validationNumber
+    validationName = $('#validation_name_r')
+    validationEmail = $('#validation_email_r')
+    validationPassword = $('#validation_password_r')
+    validationNumber = $('#validation_number_r')
+ 
     //validation control
     //NAME SURNAME
     if (nameInput == '') {
       $(validationName).html('Adı veya Soyadı boş geçilemez ...');
     }
 
-    //NUMBER
-    if (telNumberInput == '') {
-      $(validationNumber).html('Telefon Boş geçilemez ...');
-    }
+     //PASSWORD
+     if (passwordInput == '') {
+        $(validationPassword).html('Şifre Boş geçilemez ...');
+      }
 
     //EMAİL
     if (emailInput == '') {
@@ -97,28 +97,28 @@ $(function () {
       $(validationEmail).html('Email uygun formatta yazmadınız ...');
     }
 
-    //PASSWORD
-    if (passwordInput == '') {
-      $(validationPassword).html('Şifre boş geçilemez ...');
-    } else if (!validatePhone(passwordInput)) {
-      $(validationPassword).html('Şifreyi uygun formatta yazmadınız ...');
+    //TEL NUMBER
+    if (telNumberInput == '') {
+      $(validationNumber).html('Telefon numarası boş geçilemez ...')
+    } else if (!validatePhone(validationNumber)) {
+      $(validationNumber).html('Telefon numarası uygun formatta yazmadınız ...');
     }
 
+
     //regex Email
-    function validateEmail(email) {
-      var regex44 = new RegExp('^[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}$');
-      return regex44.test(email);
+    function validateEmail(email){
+        var regex=/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return regex.test(email);
     }
+
 
     //regex number
     //https://regexr.com/3c53v
-    function validatePhone(telephone) {
-      let regex3 = new RegExp(
-        '^[+]?[(]?[0-9]{3}[)]?[-s.]?[0-9]{3}[-s.]?[0-9]{4,6}$'
-      );
-
-      //let deneme=regex.test(telephone);
-      return regex3.test(telephone);
+    function validatePhone(telephone){
+        var regex= /^\(?([0-9]{3})\)?([0-9]{3})?([0-9]{4})$/;
+        let deneme=regex.test(telephone);
+        return deneme ;
     }
-  });
+
+  })
 });
