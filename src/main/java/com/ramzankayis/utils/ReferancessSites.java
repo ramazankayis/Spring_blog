628b9ac82e11d260036123322b1072d23d4668a9
ramazankayis <kayis.ramazan@gmail.com>
2022-03-27 11:26:07 +0300

    added shortcut structure

---
+++ src/main/java/com/ramzankayis/utils/ReferancessSites.java
@@ -0,0 +1,10 @@
+package com.ramzankayis.utils;
+
+public class ReferancessSites {
+
+//    https://www.geeksforgeeks.org/
+//    https://www.hackerrank.com/
+//    LeetCode - The World's Leading Online Programming Learning Platform
+//    https://www.javatpoint.com/java-tutorial
+
+}
