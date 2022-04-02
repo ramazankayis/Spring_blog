b5beba0f190e7b108ad306a584846e3ff9007959
ramazankayis <kayis.ramazan@gmail.com>
2022-04-02 09:41:23 +0300

    String trim,startsWith,endsWith

--- src/main/java/javase/_03_String.java
+++ src/main/java/javase/_03_String.java
@@ -10,5 +10,16 @@ public class _03_String {
 
         String kelime2= "javaSe";
         System.out.println(kelime.replace(kelime,kelime2));
+        //trim: kelimenin sadece başındaki ve sonundaki boşlukları siler
+        System.out.println(kelime.length());
+        System.out.println(kelime.trim().length());
+
+        //startsWith ==> Bununla mı başlıyor ? evet:true hayır:false
+        System.out.println("Bununla mı başlıyor: "+kelime.startsWith("j"));
+
+        //endsWith ==> Bununla mı bitiyor ? evet:true hayır:false
+        System.out.println("Bununla mı bitiyor:  "+kelime.endsWith("s"));
+
+
     }
 }
